873c0e1737fac820ed6ce8c62d1d853bf0ba1dc7
liaozhenyu_brian <brianliu1229@outlook.com>
2020-12-04 10:17:38 +0800

    express hello (node js) - simple get endpoint. - serve static files.

---
+++ node-hello/express-hello/main.js
@@ -0,0 +1,23 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+let moment = require('moment');
+
+let app = express();
+let port = 8080;
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({extended:true})); // to enable form-urlencoded
+
+app.use(express.static('public'));
+
+app.get('/api/1.0/ServerTime', (req, res, next)=>{
+  let payload = {
+    value: moment().format()
+  };
+  res.append('Content-Type', 'application/json');
+  res.end(JSON.stringify(payload));
+});
+
+app.listen(port, ()=>{
+  console.log('server started at port=[%d].', port);
+});
