cf761897aace7eb022e3c47956bb9e032f68fb69
liaozhenyu_brian <brianliu1229@outlook.com>
2021-06-16 15:19:13 +0800

    clean up

--- spring-cloud-config-hello/config-client/src/main/java/com/yu/Application.java
+++ spring-cloud-config-hello/config-client/src/main/java/com/yu/Application.java
@@ -1,14 +1,8 @@
 package com.yu;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan
 @SpringBootApplication
 public class Application {
 
