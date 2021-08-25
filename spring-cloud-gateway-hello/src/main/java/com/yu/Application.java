76b4e09926762a033db622a0964d65866f4a9f82
liaozhenyu_brian <brianliu1229@outlook.com>
2021-08-25 16:55:29 +0800

    spring-cloud-gateway hello - route with rate limiter

--- spring-cloud-gateway-hello/src/main/java/com/yu/Application.java
+++ spring-cloud-gateway-hello/src/main/java/com/yu/Application.java
@@ -2,10 +2,22 @@ package com.yu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public KeyResolver customKeyResolver(){
+        return exchange -> {
+            // dummy implementation that limit the rate by target path
+            return Mono.just(""
+                    +exchange.getRequest().getPath());
+        };
     }
 }
