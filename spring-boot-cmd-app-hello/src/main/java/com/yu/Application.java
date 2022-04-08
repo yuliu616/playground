1e26126aa429b5c3193331109f22e03570015589
liaozhenyu_brian <brianliu1229@outlook.com>
2022-04-08 14:47:00 +0800

    hello for command line app by spring-boot

---
+++ spring-boot-cmd-app-hello/src/main/java/com/yu/Application.java
@@ -0,0 +1,22 @@
+package com.yu;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+    public static Logger logger = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        logger.info("hello");
+    }
+
+}
