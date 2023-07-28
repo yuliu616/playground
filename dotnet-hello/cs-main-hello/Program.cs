7d70a41d9618ea7a9a71a0ba773f24bdd6b97f56
liaozhenyu_brian <brianliu1229@outlook.com>
2023-07-28 11:36:17 +0800

    dotnet(c#) hello for simple static main

---
+++ dotnet-hello/cs-main-hello/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace hello
+{
+
+  class Program
+  {
+    static string LogTime { get { return DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss.fff"); } }
+
+    static void Main(string[] args)
+    {
+      Console.WriteLine($"{LogTime} Hello, this is a dot-net cli app.");
+    }
+  }
+
+}
